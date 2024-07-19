e3538784aa230440144b168ea9e069ec0c05c44a
Mauro Telles <etasks2001@gmail.com>
2024-07-19 00:07:27 -0300

    finalizado EstadosECicloDeVidaTest.java

--- src/test/java/org/jpaexp/ecommerce/conhecendoentitymanager/EstadosECicloDeVidaTest.java
+++ src/test/java/org/jpaexp/ecommerce/conhecendoentitymanager/EstadosECicloDeVidaTest.java
@@ -1,4 +1,33 @@
+package org.jpaexp.ecommerce.conhecendoentitymanager;
+
+import org.jpaexp.ecommerce.EntityManagerTest;
+import org.jpaexp.ecommerce.model.Categoria;
+import org.jpaexp.ecommerce.model.Cliente;
+import org.jpaexp.ecommerce.model.ClienteTest;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class EstadosECicloDeVidaTest {
-  
+
+public class EstadosECicloDeVidaTest extends EntityManagerTest {
+
+    @Test
+    public void analisarEstados(){
+        Categoria categoriaNovo = new Categoria();//Estado TRANSIENTE
+        Categoria categoriaGerenciadaMerge = entityManager.merge(categoriaNovo);//Estado GERENCIADA somente o categoriaGerenciadaMerge
+
+        Cliente clienteGerenciada = entityManager.find(Cliente.class, 1);//Estado GERENCIADO
+
+        entityManager.remove(clienteGerenciada); // Estado REMOVED
+
+        entityManager.persist(clienteGerenciada);//Estado GERENCIADO
+
+entityManager.detach(clienteGerenciada);// Estado DESANEXADA
+
+
+
+
+
+
+
+    }
 }
