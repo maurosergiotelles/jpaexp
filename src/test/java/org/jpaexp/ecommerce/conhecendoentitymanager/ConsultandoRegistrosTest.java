b0f90b13f7c8734fa2a36a2e64f742f661f024fb
Mauro Telles <etasks2001@gmail.com>
2024-07-19 01:00:18 -0300

    completado o arquivo ConsultandoRegistrosTest.java

--- src/test/java/org/jpaexp/ecommerce/conhecendoentitymanager/ConsultandoRegistrosTest.java
+++ src/test/java/org/jpaexp/ecommerce/conhecendoentitymanager/ConsultandoRegistrosTest.java
@@ -1,4 +1,22 @@
+package org.jpaexp.ecommerce.conhecendoentitymanager;
+
+import org.jpaexp.ecommerce.EntityManagerTest;
+import org.jpaexp.ecommerce.model.Produto;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class ConsultandoRegistrosTest {
-  
+
+public class ConsultandoRegistrosTest extends EntityManagerTest {
+    @Test
+    public void verificarCache(){
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(produto.getNome());
+        System.out.println("----------------");
+
+        entityManager.clear();//limpa o cache de primeiro nível
+        Produto produtoResgatado = entityManager.find(Produto.class, produto.getId());//Busca na memória
+        System.out.println(produto.getNome());
+
+    }
+
 }
