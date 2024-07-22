43c2743fe728c0b9def835ff19df339d31b1e7cb
Mauro Telles <etasks2001@gmail.com>
2024-07-21 22:30:35 -0300

    criados arquivos de chaves composta

---
+++ src/test/java/org/jpaexp/ecommerce/model/mapeamentoavancado/ChaveCompostaTest.java
@@ -0,0 +1,9 @@
+package org.jpaexp.ecommerce.model.mapeamentoavancado;
+
+import org.jpaexp.ecommerce.EntityManagerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChaveCompostaTest extends EntityManagerTest {
+
+}
