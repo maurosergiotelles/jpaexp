2875e7ce2dbffc9bf33749df68e3e828a565784a
Mauro Telles <etasks2001@gmail.com>
2024-07-21 22:30:53 -0300

    competado testes de chave composta

--- src/main/java/org/jpaexp/ecommerce/model/ItemPedido.java
+++ src/main/java/org/jpaexp/ecommerce/model/ItemPedido.java
@@ -1,32 +1,40 @@
 package org.jpaexp.ecommerce.model;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Getter
 @Setter
-@EqualsAndHashCode(of = {"id"})
+//@EqualsAndHashCode(of = {"id"})
+@IdClass(ItemPedidoId.class)
 @Entity
 @Table(name = "item_pedido")
 public class ItemPedido {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @Column(name = "pedido_id")
+    private Integer pedidoId;
+
+    @Id
+    @Column(name = "produto_id")
+    private Integer produtoId;
+
+
+
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "pedido_id")
+    @JoinColumn(name = "pedido_id", insertable = false, updatable = false)
     private Pedido pedido;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "produto_id")
+    @JoinColumn(name = "produto_id", insertable = false,updatable = false)
     private Produto produto;
+
+
+
 
     @Column(name = "preco_produto")
     private BigDecimal precoProduto;
--- src/main/java/org/jpaexp/ecommerce/model/ItemPedidoId.java
+++ src/main/java/org/jpaexp/ecommerce/model/ItemPedidoId.java
@@ -1,2 +1,18 @@
-package org.jpaexp.ecommerce.model;public class ItemPedidoId {
+package org.jpaexp.ecommerce.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"pedidoId","produtoId"})
+public class ItemPedidoId implements Serializable {
+    private Integer pedidoId;
+
+    private Integer produtoId;
 }
